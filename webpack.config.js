ef84e09456cb8815b61bf40aec0982a6c2c5b6a4
HiagoKoziel <hiagokoziel100@gmail.com>
2021-01-18 18:01:58 +0100

    Adding and using plugin html

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path")
 const webpack = require("webpack")
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+
 module.exports = {
     entry: './src/client/index.js',
     module: {
@@ -8,5 +10,11 @@ module.exports = {
             exclude: /node_modules/,
             loader: "babel-loader"
         }]
-    }
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html"
+        })
+    ]
 }
